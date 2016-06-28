4b52eb8d10631682e0d6105b738c2e14f47a861a
Lukas <lorentzon.lukas@outlook.com>
2016-06-28 22:10:00 +0200

    Changed names and added new files.

--- to_watch_reminder/to_watch_reminder/MainWindow.xaml.cs
+++ to_watch_reminder/to_watch_reminder/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace to_watch_reminder
+namespace ToWatch
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
